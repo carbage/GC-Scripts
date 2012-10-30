822b4b3b6d58fe1f3e97caf2b9a77950bc598ef2
gc <awaitingvalhalla@gmail.com>
2012-10-30 09:05:13 +0000

    Moar updates

--- gcscripts/gcwarriorsguild/nodes/ThrowShotput.java
+++ gcscripts/gcwarriorsguild/nodes/ThrowShotput.java
@@ -4,11 +4,14 @@ import gcapi.constants.Areas;
 
 import org.powerbot.core.script.job.state.Node;
 import org.powerbot.game.api.methods.interactive.Players;
+import org.powerbot.game.api.methods.node.SceneEntities;
 import org.powerbot.game.api.wrappers.interactive.Player;
 
 public class ThrowShotput extends Node {
-	
+
 	Player player = Players.getLocal();
+
+	private final static int SHOTPUT_PILE_ID = 15665;
 
 	@Override
 	public boolean activate() {
@@ -17,7 +20,7 @@ public class ThrowShotput extends Node {
 
 	@Override
 	public void execute() {
-		
+		SceneEntities.getNearest(SHOTPUT_PILE_ID).click(true);
 	}
 
 }
--- gcscripts/gcwarriorsguild/nodes/Walker.java
+++ gcscripts/gcwarriorsguild/nodes/Walker.java
@@ -9,6 +9,7 @@ import org.powerbot.game.api.methods.Game;
 import org.powerbot.game.api.methods.Walking;
 import org.powerbot.game.api.methods.interactive.Players;
 import org.powerbot.game.api.methods.node.SceneEntities;
+import org.powerbot.game.api.methods.widget.Camera;
 import org.powerbot.game.api.wrappers.Tile;
 import org.powerbot.game.api.wrappers.interactive.Player;
 import org.powerbot.game.api.wrappers.node.SceneObject;
@@ -18,8 +19,6 @@ public class Walker extends Node {
 	Player player = Players.getLocal();
 	
 	private int[] STAIRCASE_IDS = { 66795, 66796, 66797 };	
-
-	private final static int SHOTPUT_PILE_ID = 15665;
 	
 	private final static int HEAVY_DOOR_ID = 15658;
 	private final static int SHOTPUT_DOOR_ID = 66758;
@@ -27,7 +26,8 @@ public class Walker extends Node {
 
 	@Override
 	public boolean activate() {
-		return Game.isLoggedIn()
+		return player.isIdle() 
+				&& Game.isLoggedIn()
 				&& !GcWarriorsGuild.isBanking
 				&& (Areas.WARRIORS_GUILD_FIRST_FLOOR.contains(player.getLocation()) 
 						|| Areas.WARRIORS_GUILD_SECOND_FLOOR.contains(player.getLocation()) 
@@ -39,26 +39,57 @@ public class Walker extends Node {
 		
 		SceneObject staircase = SceneEntities.getNearest(STAIRCASE_IDS);
 		Tile staircaseTile = LocationMethods.getObjectBox(staircase).getNearest();
+		if(staircase == null) {
+			GcWarriorsGuild.logger.log("No staircases nearby, stopping script.");
+			GcWarriorsGuild.problemFound = true;
+		}
 		
 		if (GcWarriorsGuild.collectingTokens) {
 			if (Areas.WARRIORS_GUILD_FIRST_FLOOR.contains(player.getLocation())) {
 				GcWarriorsGuild.logger.log("Player is on first floor, moving upstairs.");
 				Walking.walk(staircaseTile);
+				while (Walking.getDestination() != staircaseTile) {
+					Walking.walk(staircaseTile);
+				}
+				while (!staircase.isOnScreen()) { }
 				staircase.click(true);
 			}
 			if (Areas.WARRIORS_GUILD_SECOND_FLOOR.contains(player.getLocation())) {
 				if (player.getLocation().getX() <= 2852 && player.getLocation().getX() > 2852 && player.getPlane() == 2) {
 					GcWarriorsGuild.logger.log("Player is behind heavy door, moving through door.");
-					SceneEntities.getNearest(HEAVY_DOOR_ID).click(true);
+					SceneObject door = SceneEntities.getNearest(HEAVY_DOOR_ID);
+					if( door != null) {
+						Camera.turnTo(door);
+						if (door.isOnScreen()) {
+							door.click(true);
+						} else {
+							GcWarriorsGuild.logger.log("Could not find door, stopping.");
+							GcWarriorsGuild.problemFound = true;
+						}
+					}
 					
 				} else if (player.getLocation().getX() > 2852 && player.getPlane() == 2) {
 					GcWarriorsGuild.logger.log("Player is behind door to shotput room, moving through door.");
-					SceneEntities.getNearest(SHOTPUT_DOOR_ID).click(true);
+					SceneObject door = SceneEntities.getNearest(SHOTPUT_DOOR_ID);
+					if( door != null) {
+						Camera.turnTo(door);
+						if (door.isOnScreen()) {
+							door.click(true);
+						} else {
+							GcWarriorsGuild.logger.log("Could not find door, stopping.");
+							GcWarriorsGuild.problemFound = true;
+						}
+					}
 					
 				} else if (Areas.WARRIORS_GUILD_SHOTPUT_ROOM.contains(player.getLocation())) {
 					GcWarriorsGuild.logger.log("Player is in the shotput room, continuing to shotput range.");
-					Walking.walk(Areas.WARRIORS_GUILD_SHOTPUT_AREA.getCentralTile());
-					SceneEntities.getNearest(SHOTPUT_PILE_ID).click(true);
+					Tile tile = Areas.WARRIORS_GUILD_SHOTPUT_AREA.getCentralTile();
+					if (tile.canReach()) {
+						Walking.walk(tile);
+					} else {
+						GcWarriorsGuild.logger.log("Could not reach shotput area, stopping.");
+						GcWarriorsGuild.problemFound = true;
+					}
 				}
 			}
 			if (Areas.WARRIORS_GUILD_THIRD_FLOOR.contains(player.getLocation())) {
@@ -68,6 +99,10 @@ public class Walker extends Node {
 					SceneEntities.getNearest(CYCLOPS_DOOR_ID).click(true);
 				}
 				Walking.walk(staircaseTile);
+				while (Walking.getDestination() != staircaseTile) {
+					Walking.walk(staircaseTile);
+				}
+				while (!staircase.isOnScreen()) { }
 				staircase.click(true);
 			}
 		} else {
